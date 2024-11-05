e165451cee7e4f9f074a5919ce77d3833df6a7cd
xStephx <bojkovskistefan9@gmail.com>
2024-11-05 23:34:05 +0100

    initial setup

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                SoftRed: 'hsl(10, 79%, 65%)',
+                Cyan: 'hsl(186, 34%, 60%)',
+                DarkBrown: 'hsl(25, 47%, 15%)', 
+                MediumBrown: 'hsl(28, 10%, 53%)', 
+                Cream: 'hsl(27, 66%, 92%)',
+                VeryPaleOrange: 'hsl(33, 100%, 98%)'
+            },
+        },
+        fontFamily: {
+            DMSans: ['DM Sans', 'sans-serif'],
+        },
+    },
+    plugins: [],
+}
